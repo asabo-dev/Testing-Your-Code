cf9bec3a55bda42eb2e0a7a22932e4b9460b31ec
Efiom Ndaeyo <rylshadow@yahoo.com>
2025-03-17 22:44:33 +0800

    Adding New Tests

--- test_name_function.py
+++ test_name_function.py
@@ -1,4 +1,4 @@
-# Responding to a Failed Test
+# Adding New Tests
 
 import unittest
 from name_function import get_formatted_name
@@ -10,6 +10,11 @@ class NamesTestCase(unittest.TestCase):
         """Do names like 'Michael Scofield' work?"""
         formatted_name = get_formatted_name('Michael', 'Scofield')
         self.assertEqual(formatted_name, 'Michael Scofield')
+
+    def test_first_last_middle_name(self):
+        """Do names like 'Bola Ahmed Tinubu' work?"""
+        formatted_name = get_formatted_name('bola','tinubu' , 'ahmed')
+        self.assertEqual(formatted_name, 'Bola Ahmed Tinubu')
 
 if __name__ == '__main__':
     unittest.main()
