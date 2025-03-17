ad597f5caecfd45da42ef73c7942024fa8493252
Efiom Ndaeyo <rylshadow@yahoo.com>
2025-03-17 23:10:42 +0800

    Exercise 11-1

---
+++ Exercise_11-1/city_functions.py
@@ -0,0 +1,5 @@
+def city_country(city, country):
+    """Display a city and the country it is located."""
+    location = f"{city}, {country}."
+    return location.title()
+
---
+++ Exercise_11-1/test_city_country_function.py
@@ -0,0 +1,13 @@
+import unittest
+from city_functions import city_country
+
+class CityTestCase(unittest.TestCase):
+    """Tests for city_functions.py"""
+
+    def test_city_country_location(self):
+        """Do names like 'Lagos Nigeria' work?"""
+        formatted_name = city_country('lagos', 'nigeria')
+        self.assertEqual(formatted_name, 'Lagos, Nigeria.')
+
+if __name__ == '__main__':
+    unittest.main()
