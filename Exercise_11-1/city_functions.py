38ed808ddbf4269b852d5ece0934365cbf7ce2b0
Efiom Ndaeyo <rylshadow@yahoo.com>
2025-03-18 01:11:23 +0800

    Exercise 11-2 Pass Test with optional parameter

--- Exercise_11-1/city_functions.py
+++ Exercise_11-1/city_functions.py
@@ -1,7 +1,6 @@
-# Add a third parameter(population).
-# Modify the function so that the unit test fails.
-def city_country(city, country, population):
+# Make the third parameter optional.
+# Ensure that the unit test passes again.
+def city_country(city, country, population=''):
     """Display a city and the country it is located."""
     location = f"{city}, {country} - population {population}."
     return location.title()
-
--- Exercise_11-1/test_city_country_function.py
+++ Exercise_11-1/test_city_country_function.py
@@ -1,4 +1,4 @@
-# Fail the test
+# Pass the test with the optional parameter
 import unittest
 from city_functions import city_country
 
@@ -6,14 +6,9 @@ class CityTestCase(unittest.TestCase):
     """Tests for city_functions.py"""
 
     def test_city_country_location(self):
-        """Do names like 'Lagos Nigeria' work?"""
-        formatted_name = city_country('lagos', 'nigeria')
-        self.assertEqual(formatted_name, 'Lagos, Nigeria.')
+        """Do names like 'Lagos, Nigeria - Population 5000000.' work?"""
+        formatted_name = city_country('lagos', 'nigeria', '20,000,000')
+        self.assertEqual(formatted_name, 'Lagos, Nigeria - Population 20,000,000.')
 
 if __name__ == '__main__':
     unittest.main()
-
-"""
-OUTPUT:
-TypeError: city_country() missing 1 required positional argument: 'population'
-"""
