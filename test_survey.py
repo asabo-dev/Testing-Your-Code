ff6ff4a8254620dc897e9e0bd13c5ea47024aa99
Efiom Ndaeyo <rylshadow@yahoo.com>
2025-03-18 12:18:20 +0800

    Update the AnonymousSurvey Class.

--- test_survey.py
+++ test_survey.py
@@ -1,4 +1,4 @@
-# Testing the AnonymousSurvey Class
+# Update the AnonymousSurvey Class to accomodate more than one language.
 import unittest
 from survey import AnonymousSurvey
 
@@ -11,6 +11,16 @@ class TestAnonymousSurvey(unittest.TestCase):
         my_survey = AnonymousSurvey(question)
         my_survey.store_response('English')
         self.assertIn('English', my_survey.responses)
+
+    def test_store_three_responses(self):
+        """Test that three individual responses are stored properly."""
+        question = "What language did you first learn to speak?"
+        my_survey = AnonymousSurvey(question)
+        responses = ['English', 'Spanish', 'Ibibio']
+        for response in responses:
+            my_survey.store_response(response)
+        for response in responses:
+            self.assertIn(response, my_survey.responses)
 
 if __name__ == '__main__':
     unittest.main()
